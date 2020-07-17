8505d91bed9e82b7d898fa161ad087372ca2227d
kissjgabi <b6@b6.hu>
2020-07-17 10:41:56 +0200

    add FeedBack, TermsOfUse & PrivacyPolicy windows

--- jsB6Win.js
+++ jsB6Win.js
@@ -1,13 +1,33 @@
 let myWindow = window;
 
-function openTermsWin() {
-    myWindow.close();
-    myWindow = window.open("terms_of_use.htm", "myWindow", "_blank");
+function specsOfWindow() {
+    let border = window.outerWidth / 2 - window.innerWidth / 2;
+    let width = window.innerWidth * 0.875;
+    let height = window.innerHeight * 0.625;
+    let left = window.screenX + window.outerWidth * 0.1;
+    let top = window.screenY + window.outerHeight - height * 1.275;
+    return "width=" + width + ", height=" + height + ", top=" + top + ", left=" + left;
 }
 
-function openPolicyWin() {
+function openTermsWindow() {
     myWindow.close();
-    myWindow = window.open("privacy_policy.htm", "myWindow", "_blank");
+    let specs = specsOfWindow();
+    myWindow = window.open("https://b6.hu/terms_of_use.htm", "myWindow", specs);
+    myWindow.focus();
+}
+
+function openPolicyWindow() {
+    myWindow.close();
+    let specs = specsOfWindow();
+    myWindow = window.open("https://b6.hu/privacy_policy.htm", "myWindow", specs);
+    myWindow.focus();
+}
+
+function openFeedBackWindow() {
+    myWindow.close();
+    let specs = specsOfWindow();
+    myWindow = window.open("https://b6.hu/feedback.htm", "myWindow", specs);
+    myWindow.focus();
 }
 
 function closeWin() {
